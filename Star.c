029f67acf3cd59ba27bed8875efc04e6678775f8
Sarah Sophia Pinto <65196995+sophiapinto@users.noreply.github.com>
2020-10-13 22:40:19 -0300

    Create Star.c

---
+++ Star.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main (){
+     int cont;
+    
+     for(i = 0;i <=5; ++i){
+          
+          for(j = 0;j <= aux;++j){
+               printf("%c", num);
+          }
+          printf("\n");
+          aux = aux + 1;
+          
+     }
+     
+     return 0;
+}
